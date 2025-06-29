9a3c428d0dd50bc2e9fcf1cdf6fc68cf9022e425
danredred <weisz.danny@gmail.com>
2025-06-29 10:10:27 +0300

    Admin page filters

--- frontend/src/app/admin/admin.ts
+++ frontend/src/app/admin/admin.ts
@@ -1,24 +1,81 @@
 import { Component, computed, OnInit, signal } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
-import { ApplicationService, applicationSorter } from '../application/application.service';
+import {
+  ApplicationService,
+  applicationSorter,
+} from '../application/application.service';
 import { Application } from '../application/application';
 import { ApplicationData } from '../application/applicationData.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FormControl, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { UserProfile } from '../shared/user-profile/user-profile';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-admin',
-  imports: [Application, ScrollingModule],
+  imports: [
+    Application,
+    ScrollingModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    UserProfile,
+    MatDatepickerModule,
+  ],
+  providers:[provideNativeDateAdapter()],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
 export class Admin implements OnInit {
   private applications = signal<ApplicationData[]>([]);
-  applicationShower = computed(()=>this.applications().sort(applicationSorter))
+  filtersType = signal<'TOADMIN' | 'TOMEMBER' | undefined>(undefined);
+  filtersStatus = signal<undefined | 'PENDING' | 'CLOSED' | 'APPROVED'>(
+    undefined
+  );
+  filtersUser = signal<string>('');
+  filterStart = signal<Date|undefined>(undefined)
+  filterEnd = signal<Date|undefined>(undefined)
+
+  submittingUsersOptions = computed(() => {
+    return this.applications()
+      .map((app) => app.submitter)
+      .filter((user) =>
+        user.toLowerCase().startsWith(this.filtersUser().toLowerCase())
+      )
+      .sort((a, b) => a.length - b.length);
+  });
+
+  applicationShower = computed(() =>
+    this.applications()
+      .sort(applicationSorter)
+      .filter((app) => {
+        if (this.filtersStatus() && this.filtersStatus() !== app.status)
+          return false;
+
+        if (this.filtersType() && this.filtersType() !== app.type) return false;
+
+        if (!app.submitter.toLowerCase().startsWith(this.filtersUser().toLowerCase()))
+          return false;
+        if (this.filterStart()!==undefined && new Date( app.created)<this.filterStart()!) return false;
+        if (this.filterEnd() && new Date(app.created) > this.filterEnd()!)
+          return false;
+        return true;
+      })
+  );
   constructor(
     private authService: AuthService,
     private applicationService: ApplicationService,
-    private readonly snackBar: MatSnackBar,
+    private readonly snackBar: MatSnackBar
   ) {}
   ngOnInit(): void {
     this.applicationService.getApplications().subscribe(
@@ -45,7 +102,8 @@ export class Admin implements OnInit {
         break;
     }
     requesting.subscribe(
-      (app) => {  // updatet the applications
+      (app) => {
+        // updatet the applications
         this.applications.update((apps) => {
           apps[index] = app;
           return [...apps];
